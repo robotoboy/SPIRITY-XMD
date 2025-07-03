3867a4c20ce03012c427a7ffeab34f58f654374b
robotoboy <robotiquematique@gmail.com>
2025-07-03 01:27:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Put your Session ID here",
+SESSION_ID: process.env.SESSION_ID || "SPI~XMD~eoxTBShR#UFaNtns3RIHHKqniGyZA-oATLeZWbGgVZ6OK6aJOIaQ",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
@@ -41,7 +41,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "22603582906",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "22782257416",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝙳̷𝚊̷𝚛̷𝚔̷-𝙳̷𝙴̷𝚟̷🩸",
 // add bot owner name
